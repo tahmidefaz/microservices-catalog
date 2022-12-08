d5ed68586c16571d9915eb92f265cdd5c9a598b8
Tahmid Efaz <tefaz@redhat.com>
2022-12-08 10:08:08 -0500

    working flow interaction

--- components/Flow.js
+++ components/Flow.js
@@ -13,41 +13,63 @@ import ServiceNode from './ServiceNode';
 function Flow() {
     const [nodes, setNodes] = useState([])
     const [edges, setEdges] = useState([])
+    const [level, setLevel] = useState(1)
+    const [activeOrgs, setActiveOrgs] = useState([])
+
+    const [orgServToLoad, setOrgServToLoad] = useState('')
 
     const nodeTypes = useMemo(() => ({ orgNode: OrgNode, serviceNode: ServiceNode }), []);
 
-    const orgNodeHandler = async (orgName) => {
-        console.log("clicked...", orgName)
-        const orgServices = await GetOrgService(orgName)
-        
-        console.log("org services...", orgServices)
-
-        const servicesNodeArr = orgServices.map((service, i) => {
-            return {id: `${orgName}_${service.name}`, type: 'serviceNode', position: {x: 50+i*200, y: 50*3}, data: { name: service.name, orgName: service.org }}
-        })
-
-        const servicesEdges = orgServices.map((service, i) => {
-            console.log("org...", `${orgName}_${service.name}_edge`)
-            return { id: `${orgName}_${service.name}_edge`, source: orgName, target: `${orgName}_${service.name}`, sourceHandle: "a"}
-        })
-
-        setNodes(currNodes => [...currNodes, ...servicesNodeArr])
-        setEdges(currEdges => [...currEdges, ...servicesEdges])
-        console.log("nodes...", nodes)
+    const orgNodeHandler = (orgName) => {
+        setOrgServToLoad(orgName)
     }
+
+
+    useEffect(() => {
+        orgServToLoad !== '' &&
+        GetOrgService(orgServToLoad)
+            .then((orgServices) => {
+                if (activeOrgs.includes(orgServToLoad)) {
+                    setNodes((currNodes) => currNodes.filter(node => !(node.id).includes(`${orgServToLoad}_`)))
+                    setEdges((currEdges) => currEdges.filter(edge => !(edge.id).includes(`${orgServToLoad}_`)))
+                    setActiveOrgs(currActiveOrgs => currActiveOrgs.filter(org => org !== orgServToLoad))
+                    setLevel(currLevel => currLevel -1)
+                    setOrgServToLoad('')
+                    return
+                };
+
+                const orgName = orgServToLoad
+
+                const servicesNodeArr = orgServices.map((service, i) => {
+                    return {id: `${orgName}_${service.name}`, type: 'serviceNode', position: {x: 50+i*200, y: 50*3*level}, data: { name: service.name, orgName: service.org }}
+                })
+
+                const servicesEdges = orgServices.map((service, i) => {
+                    console.log("org...", `${orgName}_${service.name}_edge`)
+                    return { id: `${orgName}_${service.name}_edge`, source: orgName, target: `${orgName}_${service.name}`, sourceHandle: "a"}
+                })
+
+                setLevel(currLevel => currLevel+1)
+                setNodes(currNodes => [...currNodes, ...servicesNodeArr])
+                setEdges(currEdges => [...currEdges, ...servicesEdges])
+                setActiveOrgs(currActiveOrgs => [...currActiveOrgs, orgServToLoad])
+
+                setOrgServToLoad('')
+            })
+    }, [orgServToLoad])
 
     useEffect(() => {
         const all_orgs = GetAllOrgs()
             .then((data) => {
                 const nodeArr = data.map((org, i) => {
-                    return {id: `${org.name}`, type: 'orgNode', position: {x: 300+i*200, y: 50}, data: { name: org.name, handler: orgNodeHandler }}
+                    return {id: `${org.name}`, type: 'orgNode', position: {x: 300+i*200, y: 50*level}, data: { name: org.name, handler: orgNodeHandler }}
                 });
                 setNodes(nodeArr);
             });
     }, [])
 
     return (
-        <div style={{ height: '90vh' }}>
+        <div style={{ height: '100%' }}>
         <ReactFlow nodeTypes={nodeTypes} nodes={nodes} edges={edges}>
             <Background />
             <Controls />
