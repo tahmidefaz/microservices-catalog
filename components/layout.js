5c479eb5a6a28394b460045dfc3d404f78c71256
Tahmid Efaz <tefaz@redhat.com>
2022-12-08 15:32:36 -0500

    updating layout to accomodate react flow container

--- components/layout.js
+++ components/layout.js
@@ -5,23 +5,30 @@ import Header from "./header";
 import Search from "./search";
 import ServiceInfoModal from "../components/ServiceInfoModal";
 
+import styles from "../styles/Home.module.css"
 
-export default function Layout(){
+
+export default function Layout() {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [serviceModalData, setServiceModalData] = useState({name: "", org: "", description: "", onboarding_doc: ""})
 
   return(
-    <div className="pf-l-grid">
-      <div className="pf-l-grid__item pf-m-12-col" id="title">
-        <Header />
-      </div>
-      <div className="pf-l-grid__item pf-m-10-col">
-        <div style={{width: "78vw", height: "80vh"}}>
-          <Flow setServiceModalOpen={setIsModalOpen} setServiceModalData={setServiceModalData}/>
+      <div className={styles.appContainer}>
+        <div className={styles.headerBar}>
+          <Header/>
         </div>
+        <div className={styles.appContent}>
+          <div className={styles.flowContainer}>
+            <Flow setServiceModalOpen={setIsModalOpen} setServiceModalData={setServiceModalData}/>
+          </div>
+          <div className={styles.sidebar}>
+            <div className={styles.searchInput}>
+              <Search/>
+            </div>
+            <div className={styles.searchResults}>Search Result</div>
+          </div>
+        </div>
+        <ServiceInfoModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} serviceInfoData={serviceModalData} />
       </div>
-      <div className="pf-l-grid__item pf-m-2-col"><Search/></div>
-      <ServiceInfoModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} serviceInfoData={serviceModalData} />
-    </div>
-  );
+  )
 }
